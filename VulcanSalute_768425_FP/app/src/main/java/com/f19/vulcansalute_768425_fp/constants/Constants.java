a6df596c98354cf6a51f2f113e60a8052642bbaa
otetLopez <otet.tud@gmail.com>
2019-12-11 02:30:17 -0500

    Add more constants for code readability

--- VulcanSalute_768425_FP/app/src/main/java/com/f19/vulcansalute_768425_fp/constants/Constants.java
+++ VulcanSalute_768425_FP/app/src/main/java/com/f19/vulcansalute_768425_fp/constants/Constants.java
@@ -11,9 +11,20 @@ public class Constants {
 
     public static final String[] EmployeeType = {"Choose a type", "Manager", "Tester", "Programmer"};
     public static final String[] GainFactor = {"# clients", "# bugs", "# projects"};
+
+    public static final int FIELD_NOT_SET = -1;
+    public static final boolean BOOLEAN_FIELD_NOT_SET = false;
+
+
     public static final int EMPLOYMENT_CODE_MANAGER = 1;
     public static final int EMPLOYMENT_CODE_TESTER = 2;
     public static final int EMPLOYMENT_CODE_PROGRAMMER = 3;
+
+    public static final int VEHICLE_TYPE_CAR = 1;
+    public static final int VEHICLE_TYPE_MOTORCYCLE = 2;
+
+    public static final boolean MOTORCYCLE_W_SIDECAR = true;
+    public static final boolean MOTORCYCLE_WOUT_SIDECAR = false;
 
     /** Intent Codes */
     public static final int INTENT_REQUEST_CODE_ADD_OK = 1;
