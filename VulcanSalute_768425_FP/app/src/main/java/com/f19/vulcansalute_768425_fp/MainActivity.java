fc591c5a48abbbe3c6c4fc2d47a585526efd7e95
otetLopez <otet.tud@gmail.com>
2019-12-12 02:46:52 -0500

    Remove list view in Main Activity, we will soon replace it to recycler view

--- VulcanSalute_768425_FP/app/src/main/java/com/f19/vulcansalute_768425_fp/MainActivity.java
+++ VulcanSalute_768425_FP/app/src/main/java/com/f19/vulcansalute_768425_fp/MainActivity.java
@@ -1,18 +1,25 @@
 package com.f19.vulcansalute_768425_fp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.f19.vulcansalute_768425_fp.constants.Constants;
+import com.f19.vulcansalute_768425_fp.src.Employee;
 import com.f19.vulcansalute_768425_fp.src.Simulator;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Employee> employees = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +32,12 @@ public class MainActivity extends AppCompatActivity {
             simulator.run();
         }
 
-        ListView listView = findViewById(R.id.list_names);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                
-            }
-        });
+//        ListView listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//            }
+//        });
 
         Button btn = findViewById(R.id.btn_add);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +47,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, Constants.INTENT_REQUEST_CODE_ADD_OK);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Constants.INTENT_REQUEST_CODE_ADD_OK) {
+
+        }
+//        ArrayList<String> names = new ArrayList<>();
+//        for(int i=0; i<persons.size(); ++i) {
+//            names.add(persons.get(i).getFname() + " " + persons.get(i).getLname());
+//        }
+
 
     }
 }
