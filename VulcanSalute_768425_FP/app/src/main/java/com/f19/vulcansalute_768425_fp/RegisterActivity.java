573c88cb0c88d70befcd6eeab18eb7d77adde6c9
otetLopez <otet.tud@gmail.com>
2019-12-12 02:46:19 -0500

    Add checking if filled are filled a

--- VulcanSalute_768425_FP/app/src/main/java/com/f19/vulcansalute_768425_fp/RegisterActivity.java
+++ VulcanSalute_768425_FP/app/src/main/java/com/f19/vulcansalute_768425_fp/RegisterActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.f19.vulcansalute_768425_fp.constants.Constants;
+import com.f19.vulcansalute_768425_fp.src.Car;
+import com.f19.vulcansalute_768425_fp.src.Motorcycle;
+import com.f19.vulcansalute_768425_fp.src.Vehicle;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,9 +27,12 @@ public class RegisterActivity extends AppCompatActivity {
     TableRow sideCarRow;
     TextView nb;
 
+    boolean missingflds;
     short vehicleType;
     boolean sidecar;
     short employeeType;
+    short vehicleColor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         initialize();
 
-        Spinner spinnerEType = findViewById(R.id.spinner_etype);
+        final Spinner spinnerEType = findViewById(R.id.spinner_etype);
         nb = findViewById(R.id.txtview_nb);
         spinnerEType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -49,9 +58,100 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+
+        final Spinner spinnerColor = findViewById(R.id.spinner_vcolor);
+        spinnerColor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i > 0)
+                    vehicleColor = (short) (i-1);
+                else
+                    vehicleColor = Constants.FIELD_NOT_SET;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        final EditText fnametxt = findViewById(R.id.edittxt_fname);
+        final EditText lnametxt = findViewById(R.id.edittxt_lname);
+        final EditText byeartxt = findViewById(R.id.edittxt_byear);
+        final EditText salarytxt = findViewById(R.id.edittxt_msalary);
+        final EditText oratetxt = findViewById(R.id.edittxt_orate);
+        final EditText idtxt = findViewById(R.id.edittxt_eid);
+        // Employee Spinner
+        final EditText nbtxt = findViewById(R.id.edittxt_nb);
+        //Radio Vehicle Type
+        final EditText ctypetxt = findViewById(R.id.edittxt_ctype);
+        //Radio Sidecar
+        final EditText modeltxt = findViewById(R.id.edittxt_vmodel);
+        final EditText pnumbertxt = findViewById(R.id.edittxt_pnumber);
+        //Vehicle Color Spinner
+
+        Button btn = findViewById(R.id.btn_reg);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if(!checkMissingFields()) {
+
+                //}
+                String message = "";
+                if(fnametxt.getText().toString().isEmpty())
+                    message += "First Name";
+                if(lnametxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "Last Name" : ", Last Name";
+                if(byeartxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "Birth Year" : ", Birth Year";
+                if(salarytxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "Monthly Salary" : ", Monthly Salary";
+                if(oratetxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "Occupation Rate" : ", Occupation Rate";
+                if(idtxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "ID" : ", ID";
+                if(employeeType == Constants.FIELD_NOT_SET)
+                    message += message.isEmpty() ? "Employee Type" : ", Employee Type";
+                else {
+                    if(nbtxt.getText().toString().isEmpty())
+                        message += message.isEmpty() ? "Number of contributions" : ", Number of contributions";
+                }
+                if(vehicleType == Constants.FIELD_NOT_SET)
+                    message += message.isEmpty() ? "Vehicle Type" : ", Vehicle Type";
+                else {
+                    if(vehicleType == Constants.VEHICLE_TYPE_CAR) {
+                        if(ctypetxt.getText().toString().isEmpty())
+                            message += message.isEmpty() ? "Car Type" : ", Car Type";
+                    }
+                    else if(vehicleType == Constants.VEHICLE_TYPE_MOTORCYCLE) {
+                        if(sidecar == Constants.BOOLEAN_FIELD_NOT_SET)
+                            message += message.isEmpty() ? "Side Car" : ", Side Car";
+                    }
+                }
+                if(modeltxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "Model" : ", Model";
+                if(pnumbertxt.getText().toString().isEmpty())
+                    message += message.isEmpty() ? "Plate Number" : ", Plate Number";
+                if(vehicleColor == Constants.FIELD_NOT_SET)
+                    message += message.isEmpty() ? "Vehicle Color" : ", Vehicle Color";
+
+                if(!message.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Make sure the following fields are filled : " + message, Toast.LENGTH_LONG).show();
+                else {
+                    Vehicle v = (vehicleType == Constants.VEHICLE_TYPE_CAR) ?
+                            new Car(modeltxt.getText().toString(), pnumbertxt.getText().toString(), Constants.Colors[vehicleColor], ctypetxt.getText().toString()) :
+                            new Motorcycle(modeltxt.getText().toString(), pnumbertxt.getText().toString(), Constants.Colors[vehicleColor], sidecar);
+                }
+
+
+
+            }
+        });
     }
 
     private void initialize() {
+        missingflds = false;
+
         nbRow.setVisibility(View.GONE);
         carTypeRow.setVisibility(View.GONE);
         sideCarRow.setVisibility(View.GONE);
@@ -59,7 +159,10 @@ public class RegisterActivity extends AppCompatActivity {
         employeeType = Constants.FIELD_NOT_SET;
         vehicleType = Constants.FIELD_NOT_SET;
         sidecar = Constants.BOOLEAN_FIELD_NOT_SET;
+        vehicleColor = Constants.FIELD_NOT_SET;
     }
+
+
 
     private void setEmploymentType(int i) {
         if(i > 0 && i < Constants.EmployeeType.length) {
